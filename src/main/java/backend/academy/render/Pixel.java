311b71d6d38c5202590a2012632255f8b0c69a05
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-12-09 22:57:27 +0300

    refactor: Add equals and hashcode methods

--- src/main/java/backend/academy/render/Pixel.java
+++ src/main/java/backend/academy/render/Pixel.java
@@ -1,12 +1,14 @@
 package backend.academy.render;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Pixel {
     private int r;
     private int g;
