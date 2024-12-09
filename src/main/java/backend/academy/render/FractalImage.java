ca9d9f30b23a07f0570a9dfa441f1264bf7eb230
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-12-09 22:51:08 +0300

    refactor(render): Remove redundant method

--- src/main/java/backend/academy/render/FractalImage.java
+++ src/main/java/backend/academy/render/FractalImage.java
@@ -9,10 +9,6 @@ public record FractalImage(Pixel[] data, int width, int height) {
         return new FractalImage(pixels, width, height);
     }
 
-    public boolean contains(int x, int y) {
-        return x >= 0 && x < width && y >= 0 && y < height;
-    }
-
     public Pixel pixel(int x, int y) {
         return data[x + y * width];
     }
