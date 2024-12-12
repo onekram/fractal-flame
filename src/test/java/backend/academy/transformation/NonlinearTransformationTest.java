0eb159f39d098393eccf5a351a9b42a90e9cb9b4
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-12-13 00:29:27 +0300

    test(transformation): Add new tests for nonlinears

--- src/test/java/backend/academy/transformation/NonlinearTransformationTest.java
+++ src/test/java/backend/academy/transformation/NonlinearTransformationTest.java
@@ -2,7 +2,7 @@ package backend.academy.transformation;
 
 import backend.academy.render.shapes.Point;
 import java.util.stream.Stream;
-import org.assertj.core.data.Percentage;
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,7 +15,15 @@ public class NonlinearTransformationTest {
         return Stream.of(
             Arguments.of(NonlinearTransformation.LINEAR, new Point(10, 10), new Point(10, 10)),
             Arguments.of(NonlinearTransformation.SINUSOIDAL, new Point(Math.PI / 2, Math.PI / 2), new Point(1, 1)),
-            Arguments.of(NonlinearTransformation.SPHERICAL, new Point(1, 1), new Point(0.5, 0.5))
+            Arguments.of(NonlinearTransformation.SPHERICAL, new Point(1, 1), new Point(0.5, 0.5)),
+            Arguments.of(NonlinearTransformation.DIAMOND, new Point(1, Math.sqrt(3)),
+                new Point(Math.sqrt(3) * 0.5 * Math.cos(2), 0.5 * Math.sin(2))),
+            Arguments.of(NonlinearTransformation.HYPERBOLIC, new Point(1, Math.sqrt(3)),
+                new Point(Math.sqrt(3) * 0.25, 1)),
+            Arguments.of(NonlinearTransformation.DISC, new Point(1, Math.sqrt(3)), new Point(0, 0.333)),
+            Arguments.of(NonlinearTransformation.POLAR, new Point(1, Math.sqrt(3)), new Point(0.33333, 1)),
+            Arguments.of(NonlinearTransformation.TANGENT, new Point(0, 0), new Point(0, 0)),
+            Arguments.of(NonlinearTransformation.SWIRL, new Point(1, 0), new Point(Math.sin(1), Math.cos(1)))
         );
     }
 
@@ -24,8 +32,8 @@ public class NonlinearTransformationTest {
     @DisplayName("Correctness test")
     void correctnessTest(NonlinearTransformation func, Point point, Point expected) {
         Point res = func.apply(point);
-        assertThat(res.x()).isCloseTo(expected.x(), Percentage.withPercentage(1));
-        assertThat(res.y()).isCloseTo(expected.y(), Percentage.withPercentage(1));
+        assertThat(res.x()).isCloseTo(expected.x(), Offset.offset(0.01));
+        assertThat(res.y()).isCloseTo(expected.y(), Offset.offset(0.01));
     }
 
     @Test
